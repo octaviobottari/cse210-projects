1ade9491317ee63a136a84c428e0ae2559f76a2b
octaviobottari <octaviobottari7@gmail.com>
2024-11-14 17:01:39 -0300

    Learning Activity 02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Create first job instance and set its properties
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        // Create second job instance and set its properties
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        // Create resume instance and set the name
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+
+        // Add jobs to the resume's job list
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Display the resume
+        myResume.Display();
     }
 }
